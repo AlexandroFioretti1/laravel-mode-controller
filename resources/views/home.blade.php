91fdd7454db3b357811d121832421498d7e58671
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-05-24 16:34:51 +0200

    add film from DB Movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,22 @@
 @extends("layouts.app")
 
 @section("content")
-<div class="container pt-5 text-center">
-    <div class="row">
-        <div class="col">
-            <h1>funziona</h1>
+<div class="container pt-2 ">
+    <div class="row g-3">
+        @foreach($movies as $movie)
+        <div class="col-4 ">
+            <div class="card h-100">
+                <div class="card-header">
+                    <h1>{{ $movie -> title }}</h1>
+                </div>
+                <div class="card-body">
+                    <h5>{{ $movie -> original_title }}</h5>
+                    <span class="d-block">{{ $movie -> nationality }}</span>
+                    <span class="d-block">{{ $movie -> date }}</span>
+                </div>
+            </div>
         </div>
+        @endforeach
     </div>
 </div>
 

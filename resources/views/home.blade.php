c15aa7e814334e52e7cbfa2c59282c51333afac4
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-05-24 16:09:19 +0200

    add home.blade + app.blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends("layouts.app")
+
+@section("content")
+<div class="container pt-5 text-center">
+    <div class="row">
+        <div class="col">
+            <h1>funziona</h1>
+        </div>
+    </div>
+</div>
+
+@endsection
